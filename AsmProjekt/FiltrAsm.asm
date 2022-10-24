643d21ab730b4d5ea9b04874f2fdf17ac399b78e
Marcin Mitręga <m.mitrega27@gmail.com>
2022-10-24 21:38:54 +0200

    idk

--- AsmProjekt/FiltrAsm.asm
+++
@@ -1,9 +0,0 @@
-.code
-
-AsmProc proc
-add RCX, RDX
-mov RAX, RCX
-ret
-AsmProc endp
-
-end
--- CppProjekt/FiltrCpp.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-
-extern "C" __declspec(dllexport) int CppProc(int x, int y) {
-
-	return x + y;
-
-}
