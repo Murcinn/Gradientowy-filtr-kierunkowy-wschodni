117869beed30bb4161195ca067b911785cd4f4d4
Marcin Mitręga <m.mitrega27@gmail.com>
2022-11-08 00:13:40 +0100

    i  d  k rgbcpp

--- CppProjekt/FiltrCpp.cpp
+++ CppProjekt/FiltrCpp.cpp
@@ -1,7 +1,104 @@
+//#define DllExport extern "C" __declspec(dllexport)
 #include "pch.h"
+#include <vector>
+#include <iostream>
+
+
+
 
 extern "C" __declspec(dllexport) int CppProc(int x, int y) {
 
 	return x + y;
+    
+}
+
+
+
+//extern "C" __declspec(dllexport) int changeBitmap(Bitmap bmp) {
+//
+//    for (int x = 0; x < bmp.Width; x++) {
+//        for (int y = 0; y < bmp.Height; y++) {
+//
+//            Color clr = bmp.GetPixel(x, y);
+//            Color newClr = Color.FromArgb(0, clr.B, 0);
+//            bmp.SetPixel(x, y, newClr);
+//
+//        }
+//    }
+//
+//}
+
+struct ImageInfo
+{
+	unsigned char* _data;
+	int _size;
+};
+
+extern "C" __declspec(dllexport) void ChangeBitmapCpp(unsigned char* data, int dataLen, ImageInfo & imInfo) {
+   
+    std::vector<unsigned char> inputImageBytes(data, data + dataLen);
+    //dataLen = dataLen = 54;
+    std::vector<unsigned char> tempVec(dataLen);
+
+   // for (int i = 0; i < dataLen; i++) {
+        //inputImageBytes.at(i) == (255 - inputImageBytes.at(i))/2;
+   //     inputImageBytes[i] = 100;
+
+        //::cout << &inputImageBytes[i] << std::endl;
+        
+   // }
+    //std::cout << inputImageBytes.size() << " noooo";
+    //std::cout << dataLen << " noooo";
+    
+    //std::cout << inputImageBytes.at(500) << "xdd";
+
+   // inputImageBytes[1] = 100;
+   // unsigned char* output = new unsigned char[dataLen];
+
+    //for (int i = 0; i < dataLen; i++) {
+    //    output[i] = 255 - data[i];
+    //
+    //}
+   
+   // std::vector<unsigned char> tempVec(output,output+dataLen);
+   
+
+    for (size_t i = 0; i < dataLen; i += 3) {
+        inputImageBytes.at(i) = 0;
+        inputImageBytes.at(i+1) = 0;
+        inputImageBytes.at(i+2) = 0;
+    
+    
+    }
+    tempVec = inputImageBytes;
+
+	//imInfo._size = tempVec.size();
+	//imInfo._data = (unsigned char*)calloc(imInfo._size, sizeof(unsigned char));
+	//std::copy(tempVec.begin(), tempVec.end(), imInfo._data);
+    
+    
+
+
+}
+
+
+extern "C" __declspec(dllexport) bool ReleaseMemoryCpp(unsigned char* buf)
+{
+    if (buf == NULL)
+        return false;
+
+    free(buf);
+    return true;
+}
+
+
+extern "C" __declspec(dllexport) void ChangeBitmapCpp1(BYTE * *pixelValues, int width, int height) {
+
+for (size_t i = 0; i < width * height; i++) {
+
+    (*pixelValues)[i] = 255 - (*pixelValues)[i];
+
+}
+
 
 }
--- JaProjektFiltr/Program.cs
+++ JaProjektFiltr/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.Threading;
 using System.Diagnostics;
+using System.IO;
+//using static System.Net.Mime.MediaTypeNames;
+
 
 namespace JaProjektFiltr
 {
@@ -14,9 +17,48 @@ namespace JaProjektFiltr
     //internal
     internal class Program
     {
-        [DllImport(@"D:\JaProjektFiltr\x64\Debug\AsmProjekt.dll")]
+
+
+        [DllImport(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\x64\Debug\CppProjekt.dll")]
+        public static extern void ChangeBitmapCpp(byte[] _data, int _dataLen, ref ImageInfo imTemplate);
+
+        [DllImport(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\x64\Debug\CppProjekt.dll")]
+        public static extern void ReleaseMemoryCpp(IntPtr buf);
+       
+        //public struct ImageInfo
+        //{
+        //    public IntPtr _data;
+        //    public int _size;
+        //}
+
+        //public static Image ConvertImage(Image image)
+        //{
+        //    MemoryStream convertedImageMemoryStream;
+        //    using (MemoryStream sourceImageStream = new MemoryStream())
+        //    {
+        //        image.Save(sourceImageStream, System.Drawing.Imaging.ImageFormat.Bmp);
+        //        byte[] sourceImagePixels = sourceImageStream.ToArray();
+        //        ImageInfo imInfo = new ImageInfo();
+        //        ChangeBitmapCpp(sourceImagePixels, sourceImagePixels.Count(), ref imInfo);
+
+        //        byte[] imagePixels = new byte[imInfo._size];
+        //        Marshal.Copy(imInfo._data, imagePixels, 0, imInfo._size);
+        //        if (imInfo._data != IntPtr.Zero)
+        //            ReleaseMemoryCpp(imInfo._data);
+        //        convertedImageMemoryStream = new MemoryStream(imagePixels);
+        //        image.Save(convertedImageMemoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                
+                
+        //    }
+            
+        //    Image processed = new Bitmap(convertedImageMemoryStream);
+        //    //processed.Save("C:\\Users\\Marcin\\Desktop\\aaa.bmp");
+        //    return processed;
+        //}
+
+        [DllImport(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\x64\Debug\AsmProjekt.dll")]
         static extern int AsmProc(int a, int b);
-        [DllImport(@"D:\JaProjektFiltr\x64\Debug\CppProjekt.ddl")]
+        [DllImport(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\x64\Debug\CppProjekt.dll")]
         static extern int CppProc(int a, int b);
 
         private static List<Task> _tasksCpp = new List<Task>();
@@ -36,49 +78,94 @@ namespace JaProjektFiltr
 
         }
 
+        public static void changeBitmap(Bitmap bmp)
+        {
+
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+
+                    Color clr = bmp.GetPixel(x, y);
+                    Color newClr = Color.FromArgb(clr.G, clr.B, 0);
+                    bmp.SetPixel(x, y, newClr);
+
+                }
+            }
+
+        }
+        //==============================================================
+
+
+        [DllImport(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\x64\Debug\CppProjekt.dll")]
+        public static extern void ChangeBitmapCpp1(out IntPtr ptr, int width, int height);
+        
+        public IntPtr Scan0
+        {
+            get;
+            set;
+        }
+        
+        public static void ConvertImage1(Bitmap finalBmp)
+        {
+            Rectangle rect = new Rectangle(0, 0, finalBmp.Width, finalBmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                finalBmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                finalBmp.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+
+
+            ChangeBitmapCpp1(out ptr, bmpData.Stride, finalBmp.Height);
+            finalBmp.UnlockBits(bmpData);
+            finalBmp.Save("C:\\Users\\Marcin\\Desktop\\aaa.bmp");
+
+        }
+
+
         static void Main(string[] args)
         {
             int tempNumOFThreads = 5;
             Random rnd = new Random();
 
-            //int x = 5, y = 3;
-            //int fir = AsmProc(x, y);
-            //// int sec = CppProc(x, y);
+            //string curDir = Directory.GetDirectories();
+            //string myPath = Environment.GetFolderPath(Environment.);
+            //Console.WriteLine(curDir);
 
-            //Console.Write("Suma Asm:");
-            //Console.WriteLine(fir);
-            ////Console.WriteLine("\n");
-
-            //// Console.Write("Suma Cpp:");
-            ////  Console.WriteLine(sec);
-
-            Bitmap bitmap = (Bitmap)Image.FromFile(@"D:\JaProjektFiltr\bmpPath.bmp");
+            Bitmap bitmap = (Bitmap)Image.FromFile(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\Temp\bmpPath.bmp");
             Console.WriteLine("Number of threads: " + tempNumOFThreads);
+            ConvertImage1(bitmap);
+            //for (int i = 0; i < tempNumOFThreads; i++) {             
+            // _tasksCpp.Add(new Task(() => runCppProc(rnd.Next(10), rnd.Next(10))));
+            //}
 
-            for (int i = 0; i < tempNumOFThreads; i++) { 
-            
-             _tasksCpp.Add(new Task(() => runCppProc(rnd.Next(10), rnd.Next(10))));
-            }
+            //for (int i = 0; i < tempNumOFThreads; i++)
+            //{
+            // _tasksAsm.Add(new Task(() => runAsmProc(rnd.Next(10), rnd.Next(10))));
+            //}
 
-            for (int i = 0; i < tempNumOFThreads; i++)
-            {
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
 
-                _tasksAsm.Add(new Task(() => runAsmProc(rnd.Next(10), rnd.Next(10))));
-            }
+            //Parallel.ForEach(_tasksCpp, (task) => task.Start());
+            //Task.WaitAll(_tasksCpp.ToArray());
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
 
-            Parallel.ForEach(_tasksCpp, (task) => task.Start());
-            Task.WaitAll(_tasksCpp.ToArray());
-            
-            
 
-            Parallel.ForEach(_tasksAsm, (task) => task.Start());
-            Task.WaitAll(_tasksAsm.ToArray());
+            //Parallel.ForEach(_tasksAsm, (task) => task.Start());
+            //Task.WaitAll(_tasksAsm.ToArray());
 
-            sw.Stop();
-            System.TimeSpan elTime = sw.Elapsed;
+            //sw.Stop();
+            //System.TimeSpan elTime = sw.Elapsed;
+            //changeBitmap(bitmap);
+
+
+
+            //ConvertImage(bitmap).Save(@"F:\GitHub\Gradientowy-filtr-kierunkowy-wschodni\Temp\output.png");
+
+
+
         }
     }
 }
